38dd0f566b701804a40c18c8462dadc3515d94e8
Matthew Aguerreberry <Matthew Aguerreberry>
2016-06-12 17:51:06 -0300

    Class HeartView

---
+++ src/view/HeartView.java
@@ -0,0 +1,11 @@
+package view;
+
+import controller.ControllerInterface;
+import model.BeatModelInterface;
+
+public class HeartView extends DJView{
+	
+	public HeartView (ControllerInterface controller, BeatModelInterface model,String name) {	
+		super(controller,model,name);
+	}	
+}
