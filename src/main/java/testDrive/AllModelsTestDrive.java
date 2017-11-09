c0ea9e5cb1f472afa1070dc8e1136323abeb1251
Agustin Trombotto <gutitrombotto@gmail.com>
2017-11-09 20:42:06 -0300

    Modifico AllModlelsTestDrive para ver checkstyle

--- src/main/java/testDrive/AllModelsTestDrive.java
+++ src/main/java/testDrive/AllModelsTestDrive.java
@@ -17,20 +17,20 @@ import main.java.view.ViewInterface;
 public class AllModelsTestDrive {
 	static HeartModel heartModel;
 
-	 public static void main (String[] args) {
-		 heartModel = HeartModel.getInstance();
-	     ControllerInterface controllerH = new HeartController(heartModel);
-	     ViewInterface vistaH = new GeneralView();
-	     vistaH.setModel(new HeartAdapter(heartModel),controllerH);
-	     
-	     BeatModelInterface modelB = new BeatModel();
-	     ControllerInterface controllerB = new BeatController(modelB);
-	     ViewInterface vistaB = new GeneralView();
-	     vistaB.setModel(modelB,controllerB);
-	     
-	     MusicalNotesModelInterface notesModel = new MusicalNotesModel();
-	     ControllerInterface controllerN = new MusicalNotesController(notesModel);
-	     ViewInterface vistaN = new GeneralView();
-	     vistaN.setModel(new MusicalNotesAdapter(notesModel),controllerN);
-	 }
+	public static void main(String[] args) {
+		heartModel = HeartModel.getInstance();
+		ControllerInterface controllerH = new HeartController(heartModel);
+		ViewInterface vistaH = new GeneralView();
+		vistaH.setModel(new HeartAdapter(heartModel),controllerH);
+
+		BeatModelInterface modelB = new BeatModel();
+		ControllerInterface controllerB = new BeatController(modelB);
+		ViewInterface vistaB = new GeneralView();
+		vistaB.setModel(modelB,controllerB);
+
+		MusicalNotesModelInterface notesModel = new MusicalNotesModel();
+		ControllerInterface controllerN = new MusicalNotesController(notesModel);
+		ViewInterface vistaN = new GeneralView();
+		vistaN.setModel(new MusicalNotesAdapter(notesModel),controllerN);
+	}
 }
