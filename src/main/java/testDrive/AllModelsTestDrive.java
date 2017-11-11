d27850473d379fdf95290fcb4716b71326453ad3
Agustin Trombotto <gutitrombotto@gmail.com>
2017-11-11 14:05:55 -0300

    Agrego error findBugs

--- src/main/java/testDrive/AllModelsTestDrive.java
+++ src/main/java/testDrive/AllModelsTestDrive.java
@@ -20,6 +20,7 @@ public class AllModelsTestDrive {
 	public static void main(String[] args) {
 			int ErrorFindBugs;
 			int FindBugs;
+			ViewInterface vista=new GeneralView();
 			heartModel = HeartModel.getInstance();
 					ControllerInterface controllerH = new HeartController(heartModel);
 			    ViewInterface vistaH = new GeneralView();
